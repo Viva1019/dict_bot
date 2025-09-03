de38d461d74cf11272f2f22ffa394dd7ea92e186
Михаил <m_anchugov@bk.ru>
2025-09-03 16:20:49 +0700

    Update init_db.py

--- db/init_db.py
+++ db/init_db.py
@@ -6,13 +6,14 @@ from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 db_config = {
-    "database": os.getenv("DB_NAME"),
-    "user": os.getenv("DB_USER"),
-    "password": os.getenv("DB_PASSWORD"),
-    "host": os.getenv("DB_HOST"),
-    "port": os.getenv("DB_PORT")
+    "database": os.getenv("POSTGRES_DB"),
+    "user": os.getenv("POSTGRES_USER"),
+    "password": os.getenv("POSTGRES_PASSWORD"),
+    "host": os.getenv("PGHOST"),
+    "port": os.getenv("PGPORT")
 }
 
 
 db = Database(db_config)
+
 
